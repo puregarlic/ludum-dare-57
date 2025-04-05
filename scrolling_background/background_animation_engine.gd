325bbc3b840439aa461414cfe3476b384a2c1fc1
Sebastian Benjamin <sebastiancbenjamin@gmail.com>
2025-04-05 11:33:59 -0700

    Animation engine complete

--- scrolling_background/background_animation_engine.gd
+++ scrolling_background/background_animation_engine.gd
@@ -2,16 +2,37 @@
 class_name BackgroundAnimationEngine
 extends Node
 
+var curves = {
+	"100": preload("res://scrolling_background/acceleration_curves/100.tres"),
+	"150": preload("res://scrolling_background/acceleration_curves/150.tres"),
+	"ramp_from_100_to_150": preload("res://scrolling_background/acceleration_curves/ramp_from_100_to_150.tres"),
+	"ramp_from_150_to_100": preload("res://scrolling_background/acceleration_curves/ramp_from_150_to_100.tres")
+}
+
 var current_background: Background
 var animation_queue = []
 
 func set_managed_background(background: Background):
 	current_background = background
+	current_background.anim_finished.connect(_animation_resolved_callback)
 	
 func remove_managed_background(background: Background):
+	animation_queue = []
 	current_background = null
+	
+func _add_animation_to_queue(curve_name: String, transition_length: float):
+	if len(animation_queue) == 0:
+		current_background.transition_speed_with_curve(curves[curve_name], transition_length)
+	animation_queue.append({ "name": curve_name, "transition_length": transition_length })
+	
+func _animation_resolved_callback():
+	if len(animation_queue) > 0:
+		current_background.transition_speed_with_curve(curves[animation_queue.front()["name"]], animation_queue.front()["transition_length"])
 
-# Animation engine functions
-# TODO animation queue with signal callbacks for completion
-func speed_up_slow_down():
-	pass
+# Animation functions
+func speed_up_slow_down(animation_length_in_seconds: float):
+	var transition_length := animation_length_in_seconds / 3
+	_add_animation_to_queue("ramp_from_100_to_150", transition_length)
+	_add_animation_to_queue("150", transition_length)
+	_add_animation_to_queue("ramp_from_150_to_100", transition_length)
+	_add_animation_to_queue("100", 0.0)
--- scrolling_background/backgrounds/background.gd
+++ scrolling_background/backgrounds/background.gd
@@ -2,57 +2,48 @@
 class_name Background
 extends Node3D
 
+signal anim_finished
+
 @export var rail_follower: PathFollow3D
-@export var travel_duration := 5.0
+@export var progress_speed := 5.0
 @export var default_curve: Curve
 
 var elapsed := 0.0
-var blend_duration := 1.0
-var blend_progress := 0.0
-var blending := false
 
-var current_curve: Curve
-var target_curve: Curve
-var blend_start_speed_curve: Curve
+var active_curve: Curve = null
+var animation_duration := 0.0
+var animation_time := 0.0
+var loop_curve := false
 
 func _ready():
-	current_curve = default_curve
+	active_curve = default_curve
 
 func _process(delta):
-	if elapsed < travel_duration:
-		var t = clamp(elapsed / travel_duration, 0.0, 1.0)
-		var speed_curve = _get_blended_speed_curve()
-		var speed_scale = speed_curve.sample(t)
-		elapsed += delta * speed_scale
+	var base_speed := 1.0 / progress_speed
+	var speed_multiplier := 1.0
 
-		t = clamp(elapsed / travel_duration, 0.0, 1.0)
-		rail_follower.progress_ratio = t
-	else:
-		elapsed = 0
+	if active_curve and (loop_curve or animation_time < animation_duration):
+		var t := 0.0
+		if animation_duration > 0.0:
+			t = clamp(animation_time / animation_duration, 0.0, 1.0)
+		else:
+			t = fmod(animation_time, 1.0)
 
-	if blending:
-		blend_progress += delta / blend_duration
-		if blend_progress >= 1.0:
-			current_curve = target_curve
-			blending = false
+		speed_multiplier = active_curve.sample(t)
+		animation_time += delta
 
-func _get_blended_speed_curve() -> Curve:
-	if !blending:
-		return current_curve
+		if !loop_curve and animation_time >= animation_duration:
+			active_curve = default_curve
+			emit_signal("anim_finished")
 
-	var blend: float = clamp(blend_progress, 0.0, 1.0)
-	var temp_curve := Curve.new()
-	var steps := 32
-	for i in steps:
-		var x = i / (steps - 1.0)
-		var y_a = blend_start_speed_curve.sample(x)
-		var y_b = target_curve.sample(x)
-		temp_curve.add_point(x, lerp(y_a, y_b, blend))
-	return temp_curve
+	elapsed += delta * base_speed * speed_multiplier
+	if elapsed >= 1.0:
+		elapsed = fmod(elapsed, 1.0)
 
-func transition_to_speed_curve(new_curve: Curve, duration := 1.0):
-	blend_start_speed_curve = current_curve
-	target_curve = new_curve
-	blend_progress = 0.0
-	blend_duration = duration
-	blending = true
+	rail_follower.progress_ratio = clamp(elapsed, 0.0, 1.0)
+
+func transition_speed_with_curve(curve: Curve, duration: float):
+	active_curve = curve
+	animation_time = 0.0
+	animation_duration = duration
+	loop_curve = (duration == 0.0)
