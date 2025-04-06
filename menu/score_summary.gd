de3b7ae094ba2e371d7f215d31084a57343b7bba
Sebastian Benjamin <sebastiancbenjamin@gmail.com>
2025-04-06 16:47:14 -0700

    End screen

---
+++ menu/score_summary.gd
@@ -0,0 +1,68 @@
+extends Node
+
+@onready var dialogues_value_label = $MarginContainer/VBoxContainer/DialoguesContainer/DialoguesValue
+@onready var fathoms_value_label = $MarginContainer/VBoxContainer/FathomsContainer/FathomsValue
+@onready var grade_value_label = $MarginContainer/VBoxContainer/GradeContainer/GradeValue
+
+@export var dialogues_completed: int = 0
+@export var next_scene: PackedScene
+@export var score_sound: AudioStream
+@export var rank_sound: AudioStream
+
+const FATHOMS_PER_DIALOGUE = 5621
+
+func _ready():
+	dialogues_value_label.text = ""
+	fathoms_value_label.text = ""
+	grade_value_label.text = ""
+	start_score_sequence()
+
+func start_score_sequence():
+	await get_tree().create_timer(1.2).timeout
+	dialogues_value_label.text = str(dialogues_completed)
+	play_sound(score_sound)
+
+	await get_tree().create_timer(1.2).timeout
+	var fathoms = dialogues_completed * FATHOMS_PER_DIALOGUE
+	fathoms_value_label.text = str(fathoms)
+	play_sound(score_sound)
+
+	await get_tree().create_timer(1.9).timeout
+	var grade = calculate_grade(dialogues_completed)
+	grade_value_label.text = grade
+	play_sound(rank_sound)
+
+	await get_tree().create_timer(5.0).timeout
+
+	var tween = create_tween()
+	tween.tween_method(
+		func(alpha): $ColorRect.color = Color(0, 0, 0, alpha),
+		0.0, 1.0, 1.5
+	)
+	await tween.finished
+	await get_tree().create_timer(1.5).timeout
+
+	if next_scene:
+		get_tree().change_scene_to_packed(next_scene)
+
+func calculate_grade(dialogues: int) -> String:
+	if dialogues < 5:
+		return "Phlebotomist"
+	elif dialogues < 10:
+		return "Scholar"
+	elif dialogues < 15:
+		return "Orator"
+	elif dialogues < 20:
+		return "Philosopher"
+	elif dialogues < 25:
+		return "The Great"
+	else:
+		return "Immortal Persuasion!"
+
+func play_sound(stream: AudioStream):
+	var player = AudioStreamPlayer.new()
+	add_child(player)
+	player.stream = stream
+	player.play()
+	var cleanup_timer = get_tree().create_timer(stream.get_length())
+	cleanup_timer.timeout.connect(func(): player.queue_free())
